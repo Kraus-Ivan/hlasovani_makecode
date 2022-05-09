7d7cb9e7aa25c98771d4de96e13259bb64f12a4b
Ivan Kraus <91247726+Kraus-Ivan@users.noreply.github.com>
2022-05-09 17:50:27 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 radio.set_group(58)
 radio.set_transmit_serial_number(True)
-vlastni_ser_cislo = control.device_serial_number()
 stav = 0 # stav 0 je klient, stav 1 je server
-volba = 0 # volba klienta
-hlasovani = False # stav, jestli je zapnuto hlasovani od serveru
+volba = 0 # volba klienta (0 = A, 1 = B...)
+hlasovani = False # udava, jestli je zapnuto hlasovani od serveru
 
-list_s_hlasy: List[any] = []
+list_s_hlasy: List[any] = [] # list s hlasy, ktere prijmul server od klientu
+rozsah = 26 # zvoleny rozsah moznosti hlasu (A, B, C...)
+
 basic.show_icon(IconNames.ASLEEP)
-
-rozsah = 26 #zvoleny rozsah moznosti hlasu
 
 def reset_promennych():
     global stav, volba, hlasovani, list_s_hlasy
@@ -20,96 +19,100 @@ def reset_promennych():
         basic.show_icon(IconNames.ASLEEP)
 
 def on_received_value(name, value):
-    global stav, list_s_hlasy, hlasovani, vlastni_ser_cislo, rozsah
-    ser_cislo = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+    global stav, list_s_hlasy, hlasovani, rozsah, volba
+    ser_cislo = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER) 
+    vlastni_ser_cislo = control.device_serial_number()
 
-    if stav == 1 and name == "answer" and hlasovani: # kdyz je stav nastaven na server a hlasovani je zapnuto
+    if stav == 1 and name == "answer" and hlasovani: # server pri zapnutem hlasovani...
         counter = 0
         nalez = False
-        for iterace in list_s_hlasy: # proje list s hlasy a pokud najde jiz objevene seriove cislo, tak nahradi odpoved
-            if iterace["ser_cislo"] == ser_cislo:
+        for hlas in list_s_hlasy: # projde list s hlasy a pokud najde jiz objevene seriove cislo, tak nahradi odpoved daneho serioveho cisla
+            if hlas["ser_cislo"] == ser_cislo:
                 list_s_hlasy[counter]["choose"] = value
                 nalez = True
             counter += 1
-        if nalez == False: # pokud nenalezne seriove cislo, ulozi hlas jako novy
-            list_s_hlasy.push({"ser_cislo" : ser_cislo, "choose" : value}) #pushnuti hlasu do listu s hlasy
+        if nalez == False: # pokud nenalezne stejne seriove cislo, ulozi hlas jako novy
+            list_s_hlasy.push({"ser_cislo" : ser_cislo, "choose" : value}) # pushnuti hlasu do listu s hlasy
         radio.send_value("ano", ser_cislo) #posle potvrzeni o prijmuti (name = "ano")
         basic.show_icon(IconNames.HEART)
         basic.clear_screen()
-    elif stav == 0 and name == "ano": #kdyz je stav nastaven na klienta a potvrzene prijmuti
-        if value == vlastni_ser_cislo: #kdyz se shoduje prijmute ser. cislo s vlastnim ser. cislem
+
+    elif stav == 0 and name == "ano": # klient prijme potvrzene prijmuti "ano"
+        if value == vlastni_ser_cislo: # kdyz se prijate seriove cislo shoduje s vlastnim, tak ukaze ikonku YES
             basic.show_icon(IconNames.YES)
             if hlasovani:
                 basic.show_string(String.from_char_code(volba+65), rozsah - 1)
+
+    elif stav == 0 and name == "stav": # klient prijme informaci o zmene stavu, value == 1 zapne hlasovani, value == 0 vypne hlasovani
+        if value == 1:
+            hlasovani = True
+            volba = 0
+            basic.show_string(String.from_char_code(volba+65), rozsah - 1)
+        else:
+            hlasovani = False
+            basic.show_icon(IconNames.ASLEEP)
 radio.on_received_value(on_received_value)
 
 
 def on_received_string(receivedString):
     global stav, hlasovani
-    if stav == 0 and receivedString == "stav": # klient pri prijmuti "stav", zmeni stav hlasovani
-        if hlasovani:
-            hlasovani = False
-            basic.show_icon(IconNames.ASLEEP)
-        else:
-            hlasovani = True
-            basic.show_string(String.from_char_code(volba+65), rozsah - 1)
-
-    elif stav == 0 and receivedString == "reset": # klient pri prijmuti "reset", vynuluje hlasovani
+    if stav == 0 and receivedString == "reset": # klient pri prijmuti "reset" resetuje hlasovani
         reset_promennych()
 radio.on_received_string(on_received_string)
 
 
 def vyhodnoceni_hlasu():
     global list_s_hlasy, rozsah
-    for moznost_hlasu in range(0, rozsah):
+    for moznost_hlasu in range(0, rozsah): # projde moznosti od 0 do zvoleneho rozsahu
         pocet = 0
-        for n in list_s_hlasy:
-            if n["choose"] == moznost_hlasu:
+        for n in list_s_hlasy: # projde hlasy od klientu v listu list_s_hlasy
+            if n["choose"] == moznost_hlasu: # pokud najde shodu moznosti s hlasem, pricte k promenne pocet 1
                 pocet += 1
-        if pocet > 0:
+        if pocet > 0: # pokud naleznul aspon 1 shodu, ukaze danou moznost a jeji pocet vyskytu
             basic.show_string(String.from_char_code(moznost_hlasu + 65))
             basic.show_number(pocet)
             basic.clear_screen()
 
-def on_logo_event_pressed():
+def on_logo_event_pressed(): # DOTYKOVE TLACITKO
     global stav, volba
-    if stav == 0: # klient odesle volbu
+    if stav == 0: # klient odesle svoji volbu
         radio.send_value("answer", volba)
     if stav == 1: # server vynuluje hlasovani
         radio.send_string("reset")
         reset_promennych()
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
 
-def on_forever(): #đdfljasdf
+
+def on_forever():
     global volba, hlasovani, stav, rozsah
-    # na zacatku: volba = 0
-    if input.button_is_pressed(Button.A):
+
+    if input.button_is_pressed(Button.A): #TLACITKO A
         if stav == 0 and hlasovani: # klient zvysi volbu o 1
             volba += 1
             volba = Math.constrain(volba, 0, rozsah - 1)
-            if hlasovani: # kvuli mozne rychle zmene hlasovani od server jeste jednou testuje klient stav hlasovani
-                basic.show_string(String.from_char_code(volba+65), 40)
+            basic.show_string(String.from_char_code(volba+65), 40)
+
         elif stav == 1:
-            radio.send_string("stav") # odesila informaci o zmene stavu hlasovani
-            if hlasovani: # server vypne hlasovani, pokud je zaple
-                hlasovani = False
+            if hlasovani:
+                radio.send_value("stav", 0) # pokud je hlasovani zapnute, posle server informaci o vypnuti hlasovani
+                hlasovani = False # server u sebe vypne hlasovani
                 basic.show_icon(IconNames.NO)
             else:
-                hlasovani = True # jinak zapne server hlasovani
+                radio.send_value("stav", 1)  # pokud je hlasovani vypnute, posle server informaci o zapnuti hlasovani
+                hlasovani = True # server u sebe zapne hlasovani
                 basic.show_icon(IconNames.YES)
             basic.clear_screen()
             
-    if input.button_is_pressed(Button.B):
-        if stav == 0: # klient snizi volbu o 1
+    if input.button_is_pressed(Button.B): #TLACITKO B
+        if stav == 0: # klient snizi svoji volbu o 1
             volba -= 1
             volba = Math.constrain(volba, 0, rozsah - 1)
-            if hlasovani: # kvuli mozne rychle zmene hlasovani od server jeste jednou testuje klient stav hlasovani
-                basic.show_string(String.from_char_code(volba+65), 40)
+            basic.show_string(String.from_char_code(volba+65), 40)
 
         elif stav == 1: # server vyhodnoti hlasovani
             vyhodnoceni_hlasu()
 
-    if input.pin_is_pressed(TouchPin.P0): # zmena z klienta na server a naopak
+    if input.pin_is_pressed(TouchPin.P0): # zmena ze stavu klienta na server a naopak
         if stav == 0:
             stav = 1
         else:
--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 radio.setGroup(58)
 radio.setTransmitSerialNumber(true)
-let vlastni_ser_cislo = control.deviceSerialNumber()
 let stav = 0
 //  stav 0 je klient, stav 1 je server
 let volba = 0
-//  volba klienta
+//  volba klienta (0 = A, 1 = B...)
 let hlasovani = false
-//  stav, jestli je zapnuto hlasovani od serveru
+//  udava, jestli je zapnuto hlasovani od serveru
 let list_s_hlasy : any[] = []
-basic.showIcon(IconNames.Asleep)
+//  list s hlasy, ktere prijmul server od klientu
 let rozsah = 26
-// zvoleny rozsah moznosti hlasu
+//  zvoleny rozsah moznosti hlasu (A, B, C...)
+basic.showIcon(IconNames.Asleep)
 function reset_promennych() {
     
     volba = 0
@@ -29,13 +29,14 @@ radio.onReceivedValue(function on_received_value(name: string, value: number) {
     let nalez: boolean;
     
     let ser_cislo = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    let vlastni_ser_cislo = control.deviceSerialNumber()
     if (stav == 1 && name == "answer" && hlasovani) {
-        //  kdyz je stav nastaven na server a hlasovani je zapnuto
+        //  server pri zapnutem hlasovani...
         counter = 0
         nalez = false
-        for (let iterace of list_s_hlasy) {
-            //  proje list s hlasy a pokud najde jiz objevene seriove cislo, tak nahradi odpoved
-            if (iterace["ser_cislo"] == ser_cislo) {
+        for (let hlas of list_s_hlasy) {
+            //  projde list s hlasy a pokud najde jiz objevene seriove cislo, tak nahradi odpoved daneho serioveho cisla
+            if (hlas["ser_cislo"] == ser_cislo) {
                 list_s_hlasy[counter]["choose"] = value
                 nalez = true
             }
@@ -43,7 +44,7 @@ radio.onReceivedValue(function on_received_value(name: string, value: number) {
             counter += 1
         }
         if (nalez == false) {
-            //  pokud nenalezne seriove cislo, ulozi hlas jako novy
+            //  pokud nenalezne stejne seriove cislo, ulozi hlas jako novy
             list_s_hlasy.push( {
                 "ser_cislo" : ser_cislo,
                 "choose" : value,
@@ -51,15 +52,15 @@ radio.onReceivedValue(function on_received_value(name: string, value: number) {
             )
         }
         
-        // pushnuti hlasu do listu s hlasy
+        //  pushnuti hlasu do listu s hlasy
         radio.sendValue("ano", ser_cislo)
         // posle potvrzeni o prijmuti (name = "ano")
         basic.showIcon(IconNames.Heart)
         basic.clearScreen()
     } else if (stav == 0 && name == "ano") {
-        // kdyz je stav nastaven na klienta a potvrzene prijmuti
+        //  klient prijme potvrzene prijmuti "ano"
         if (value == vlastni_ser_cislo) {
-            // kdyz se shoduje prijmute ser. cislo s vlastnim ser. cislem
+            //  kdyz se prijate seriove cislo shoduje s vlastnim, tak ukaze ikonku YES
             basic.showIcon(IconNames.Yes)
             if (hlasovani) {
                 basic.showString(String.fromCharCode(volba + 65), rozsah - 1)
@@ -67,23 +68,24 @@ radio.onReceivedValue(function on_received_value(name: string, value: number) {
             
         }
         
+    } else if (stav == 0 && name == "stav") {
+        //  klient prijme informaci o zmene stavu, value == 1 zapne hlasovani, value == 0 vypne hlasovani
+        if (value == 1) {
+            hlasovani = true
+            volba = 0
+            basic.showString(String.fromCharCode(volba + 65), rozsah - 1)
+        } else {
+            hlasovani = false
+            basic.showIcon(IconNames.Asleep)
+        }
+        
     }
     
 })
 radio.onReceivedString(function on_received_string(receivedString: string) {
     
-    if (stav == 0 && receivedString == "stav") {
-        //  klient pri prijmuti "stav", zmeni stav hlasovani
-        if (hlasovani) {
-            hlasovani = false
-            basic.showIcon(IconNames.Asleep)
-        } else {
-            hlasovani = true
-            basic.showString(String.fromCharCode(volba + 65), rozsah - 1)
-        }
-        
-    } else if (stav == 0 && receivedString == "reset") {
-        //  klient pri prijmuti "reset", vynuluje hlasovani
+    if (stav == 0 && receivedString == "reset") {
+        //  klient pri prijmuti "reset" resetuje hlasovani
         reset_promennych()
     }
     
@@ -92,14 +94,18 @@ function vyhodnoceni_hlasu() {
     let pocet: number;
     
     for (let moznost_hlasu = 0; moznost_hlasu < rozsah; moznost_hlasu++) {
+        //  projde moznosti od 0 do zvoleneho rozsahu
         pocet = 0
         for (let n of list_s_hlasy) {
+            //  projde hlasy od klientu v listu list_s_hlasy
             if (n["choose"] == moznost_hlasu) {
+                //  pokud najde shodu moznosti s hlasem, pricte k promenne pocet 1
                 pocet += 1
             }
             
         }
         if (pocet > 0) {
+            //  pokud naleznul aspon 1 shodu, ukaze danou moznost a jeji pocet vyskytu
             basic.showString(String.fromCharCode(moznost_hlasu + 65))
             basic.showNumber(pocet)
             basic.clearScreen()
@@ -109,9 +115,10 @@ function vyhodnoceni_hlasu() {
 }
 
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    //  DOTYKOVE TLACITKO
     
     if (stav == 0) {
-        //  klient odesle volbu
+        //  klient odesle svoji volbu
         radio.sendValue("answer", volba)
     }
     
@@ -123,29 +130,26 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
     
 })
 basic.forever(function on_forever() {
-    // đdfljasdf
     
-    //  na zacatku: volba = 0
     if (input.buttonIsPressed(Button.A)) {
+        // TLACITKO A
         if (stav == 0 && hlasovani) {
             //  klient zvysi volbu o 1
             volba += 1
             volba = Math.constrain(volba, 0, rozsah - 1)
-            if (hlasovani) {
-                //  kvuli mozne rychle zmene hlasovani od server jeste jednou testuje klient stav hlasovani
-                basic.showString(String.fromCharCode(volba + 65), 40)
-            }
-            
+            basic.showString(String.fromCharCode(volba + 65), 40)
         } else if (stav == 1) {
-            radio.sendString("stav")
-            //  odesila informaci o zmene stavu hlasovani
             if (hlasovani) {
-                //  server vypne hlasovani, pokud je zaple
+                radio.sendValue("stav", 0)
+                //  pokud je hlasovani zapnute, posle server informaci o vypnuti hlasovani
                 hlasovani = false
+                //  server u sebe vypne hlasovani
                 basic.showIcon(IconNames.No)
             } else {
+                radio.sendValue("stav", 1)
+                //  pokud je hlasovani vypnute, posle server informaci o zapnuti hlasovani
                 hlasovani = true
-                //  jinak zapne server hlasovani
+                //  server u sebe zapne hlasovani
                 basic.showIcon(IconNames.Yes)
             }
             
@@ -155,15 +159,12 @@ basic.forever(function on_forever() {
     }
     
     if (input.buttonIsPressed(Button.B)) {
+        // TLACITKO B
         if (stav == 0) {
-            //  klient snizi volbu o 1
+            //  klient snizi svoji volbu o 1
             volba -= 1
             volba = Math.constrain(volba, 0, rozsah - 1)
-            if (hlasovani) {
-                //  kvuli mozne rychle zmene hlasovani od server jeste jednou testuje klient stav hlasovani
-                basic.showString(String.fromCharCode(volba + 65), 40)
-            }
-            
+            basic.showString(String.fromCharCode(volba + 65), 40)
         } else if (stav == 1) {
             //  server vyhodnoti hlasovani
             vyhodnoceni_hlasu()
@@ -172,7 +173,7 @@ basic.forever(function on_forever() {
     }
     
     if (input.pinIsPressed(TouchPin.P0)) {
-        //  zmena z klienta na server a naopak
+        //  zmena ze stavu klienta na server a naopak
         if (stav == 0) {
             stav = 1
         } else {
